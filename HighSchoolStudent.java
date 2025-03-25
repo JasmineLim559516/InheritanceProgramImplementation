1b2b21cb59dbd00298567bcc159ccf3936b95bd4
JasmineLim559516 <142464324+JasmineLim559516@users.noreply.github.com>
2025-03-24 18:06:26 -0700

    Create HighSchoolStudent.java

---
+++ HighSchoolStudent.java
@@ -0,0 +1,64 @@
+public class HighSchoolStudent extends Student {
+    
+    private String volunteerPlace;
+    private boolean hasJob;
+    
+    public HighSchoolStudent(String name, int age, int grade, String volunteerPlace, boolean job)
+    {
+        super(name, age, grade);
+        this.volunteerPlace = volunteerPlace;
+        hasJob = job;
+    }
+    
+    public void volunteer()
+    {
+        System.out.println("I am volunteering at " + volunteerPlace);
+    }
+    
+    public void work()
+    {
+        if (hasJob)
+        {
+            System.out.println("I am working");
+        }
+        else
+        {
+            System.out.println("I don't have a job");
+        }
+    }
+    
+    public void study()
+    {
+        System.out.println("I studied for 4 hours");
+    }
+    
+    public void sleep()
+    {
+        System.out.println("I slept for 3 hours last night");
+    }
+    
+    public void leisureTime()
+    {
+        System.out.println("I've been relaxing for 1 hour");
+    }
+    
+    public String getVolunteerPlace()
+    {
+        return volunteerPlace;
+    }
+    
+    public void setVolunteerPlace(String place)
+    {
+        volunteerPlace = place;
+    }
+    
+    public boolean getVocationalState()
+    {
+        return hasJob;
+    }
+    
+    public void setVocationalState(boolean job)
+    {
+        hasJob = job;
+    }
+}
